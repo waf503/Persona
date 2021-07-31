6a384c244a9fc4d483ac6a9f2638bcbf7a4c1f7c
waf503 <54567401+waf503@users.noreply.github.com>
2021-07-31 14:51:47 -0600

    Agregando modelo persona

---
+++ Models/Persona.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Persona.Models
+{
+    public class Persona
+    {
+        public string DUI { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Direccion { get; set; }
+        public string correo { get; set; }
+    }
+
+    public class PesonaDBContext: DbContext
+    {
+        public DbSet<Persona> Personas { get; set; }
+    }
+}
